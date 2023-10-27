16630ca994894fa83c76bfeff6c91ce5d67897aa
Alexandra Asanache <ale.asanache@gmail.com>
2023-10-27 18:32:21 +0100

    Rendered Data and Changed file locations

--- weather.js
+++ weather.js
@@ -1,21 +1,21 @@
 import axios from "axios"
 
 export function getWeather(lat, lon, timezone) {
-    return axios.get("https://api.open-meteo.com/v1/forecast?current=temperature_2m,weathercode,windspeed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime", { params: {
+    return axios.get("https://api.open-meteo.com/v1/forecast?current=temperature_2m,weathercode,windspeed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime", { 
+        params: {
         latitude: lat,
         longitude: lon,
         timezone,
     },
 }
 )
-.then(({ data}) => {
-    return {
-        current: parseCurrentWeather(data),
-        daily: parseDailyWeather(data),
-        hourly: parseHourlyWeather(data),
-    }
-})
-
+    .then(({ data}) => {
+        return {
+            current: parseCurrentWeather(data),
+            daily: parseDailyWeather(data),
+            hourly: parseHourlyWeather(data),
+        }
+    })
 }
 
 function parseCurrentWeather({ current, daily }) {
